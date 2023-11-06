0cc3c5f7afe431e6bd8139db7a1e6dea5c072da6
KOMALA123Nshayan2003 <shayarukomi@gmail.com>
2023-11-06 20:05:15 +0530

    node changes

--- client/src/http.js
+++ client/src/http.js
@@ -1,33 +1,33 @@
-import axios from "axios";
+import axios from 'axios'
 
-export const BASE_URL = "http://localhost:8000/";
+export const BASE_URL = 'https://wonderrecipe.onrender.com'
 
 const http = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
   headers: {
-    "Content-Type": "application/json",
-  },
-});
+    'Content-Type': 'application/json'
+  }
+})
 
 export const AxiosPrivate = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
-  headers: { "Content-Type": "application/json" },
-});
+  headers: { 'Content-Type': 'application/json' }
+})
 
 AxiosPrivate.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("recipeToken");
+  config => {
+    const token = localStorage.getItem('recipeToken')
 
     if (token) {
-      if (!config.headers["Authorization"]) {
-        config.headers["Authorization"] = `Bearer ${token}`;
+      if (!config.headers['Authorization']) {
+        config.headers['Authorization'] = `Bearer ${token}`
       }
     }
-    return config;
+    return config
   },
-  (error) => Promise.reject(error)
-);
+  error => Promise.reject(error)
+)
 
-export default http;
+export default http
